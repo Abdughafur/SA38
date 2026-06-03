af1f9068aea0fc0a5410035e786f932ef320fee8
Abdughafur Khujzoda <abdughafur.2010.khujzoda@gmail.com>
2026-06-04 00:09:29 +0500

    Refactor slideshow functionality and controls

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,42 +2,95 @@
   Abdughafur Khujzoda
   Copyright (c) 2026 Abdughafur Khujzoda. All rights reserved.
 */
-const slides = document.querySelectorAll(".slide");
+
+const totalImages = 16;
 let index = 0;
 let playing = true;
 
-function showNext() {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
+const slidesContainer = document.getElementById("slides");
+
+for (let i = 1; i <= totalImages; i++) {
+  const img = document.createElement("img");
+  img.src = `assets/images/${i}.jpg`;
+  img.className = "slide";
+  if (i === 1) img.classList.add("active");
+  slidesContainer.appendChild(img);
+}
+
+const slides = document.querySelectorAll(".slide");
+
+const dotsContainer = document.getElementById("dots");
+
+slides.forEach((_, i) => {
+  const d = document.createElement("div");
+  d.className = "dot";
+  if (i === 0) d.classList.add("active");
+  d.onclick = () => goTo(i);
+  dotsContainer.appendChild(d);
+});
+
+const dots = document.querySelectorAll(".dot");
+
+function update() {
+  slides.forEach((s) => s.classList.remove("active"));
   slides[index].classList.add("active");
+
+  dots.forEach((d) => d.classList.remove("active"));
+  dots[index].classList.add("active");
+}
+
+function next() {
+  index = (index + 1) % slides.length;
+  update();
+}
+
+function prev() {
+  index = (index - 1 + slides.length) % slides.length;
+  update();
+}
+
+function goTo(i) {
+  index = i;
+  update();
+}
+
+function togglePlay() {
+  playing = !playing;
+
+  const icon = document.getElementById("playIcon");
+
+  if (playing) {
+    icon.innerHTML = `
+      <rect x="6" y="5" width="4" height="14" fill="#4da3ff"/>
+      <rect x="14" y="5" width="4" height="14" fill="#4da3ff"/>
+    `;
+  } else {
+    icon.innerHTML = `
+      <path d="M8 5v14l12-7-12-7z" fill="#4da3ff"/>
+    `;
+  }
 }
 
 setInterval(() => {
-  if (playing) showNext();
-}, 2000);
+  if (playing) next();
+}, 2500);
 
-const pauseBtn = document.getElementById("pauseBtn");
-const pauseIcon = document.getElementById("pauseIcon");
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
 
-pauseBtn.onclick = () => {
-  playing = !playing;
+function openInfo() {
+  document.getElementById("infoScreen").style.display = "flex";
+}
 
-  pauseIcon.innerHTML = playing
-    ? `<rect x="6" y="5" width="4" height="14" fill="#007aff"/>
-       <rect x="14" y="5" width="4" height="14" fill="#007aff"/>`
-    : `<polygon points="6,4 20,12 6,20" fill="#007aff"/>`;
-};
+function closeInfo() {
+  document.getElementById("infoScreen").style.display = "none";
+}
 
-const aboutBtn = document.getElementById("aboutBtn");
-const aboutModal = document.getElementById("aboutModal");
-const closeAbout = document.getElementById("closeAbout");
-
-aboutBtn.onclick = () => {
-  aboutModal.classList.add("show");
-  playing = false;
-};
-
-closeAbout.onclick = () => {
-  aboutModal.classList.remove("show");
-  playing = true;
-};
+/*
+  Copyright 2026 Abdughafur Khujzoda
+*/
